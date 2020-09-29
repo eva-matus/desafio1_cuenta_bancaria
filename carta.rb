6d0b61df9412207a741fedb27c058f2f33997f07
eva-matus <eva.matus@hotmail.com>
2020-09-29 18:39:44 -0300

    .include

--- carta.rb
+++ carta.rb
@@ -13,3 +13,7 @@
 class Carta
     attr_accessor :numero, :pinta
     def initialize(numero, pinta)
+        if !(1..13).include?(numero)
+            raise ArgumentError, 'El numero no esta entre 1 y 13'
+        end
+        if !['C', 'D', 'E', 'T'].include?(pinta)
