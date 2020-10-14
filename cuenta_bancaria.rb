214e5111b8d78c7118458f0552109a5aa8f5857a
eva-matus <eva.matus@hotmail.com>
2020-10-14 19:37:11 -0300

    The account number method must be instance

--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -15,15 +15,16 @@
 class CuentaBancaria
     attr_accessor :nombre_de_usuario
     def initialize(nombre_usuario, numero_de_cuenta, vip = 0)
+          @nombre_de_usuario = numero_de_cuenta
+          @numero_de_cuenta = numero_de_cuenta
+          @vip = vip
+
         if numero_de_cuenta.digits.count != 8
             raise RangeError, 'El numero de digitos es distinto a 8'
         end
         if !(0..1).include?(vip)
             raise RangeError, 'vip no es 0 o 1'
         end
-        @nombre_de_usuario = numero_de_cuenta
-        @numero_de_cuenta = numero_de_cuenta
-        @vip = vip
     end
 def numero_de_cuenta
     "#{@vip} -#{@numero_de_cuenta}"
